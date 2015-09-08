30e0fd1656d4c6889e49c7f1f1c41d09142af4fb
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-08 18:28:02 +0100

    Updated ship_spec

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,40 +1,68 @@
+# require './lib/ship'
+
+# describe Ship do
+
+#
+
+#   # it "status when created" do
+#   #   ship = Ship.new 'A1'
+#   #   expect(ship.hit).to eq false
+#   # end
+
+#   it "can be hit" do
+#     ship = Ship.new 'A1'
+#     expect(ship.hit).to eq true
+#   end
+
+
+#   # it "knows own size" do
+#   #   ship = Ship.new(2)
+#   #   expect(ship.size).to be_between(2,5).inclusive
+#   # end
+
+# end
 
 # # require 'water'
 
 class Ship
 
-attr_reader :size
+attr_reader :size, :start_position
 attr_accessor :hits
 
-  def initialize(size)
-    @size = size
-    @hits = 0
-    @floating = true
-    @sunk = false
-  end
+ def initialize(size, start_position)
+   @size = size
+   @hits = 0
+   # @floating = true
+   @sunk = false
+   @start_position = start_position
+ end
 
-  def floating?
-    @floating
-  end
-
-  def recieve_hit
-    @hits +=1
-  end
-
-  def sunk
-    @size == @hits
-    # true
-  end
-
-  # def size
-  # end
+ # def floating?
+ #   @floating
+ # end
 
 
+ def recieve_hit
+   @hits +=1
+ end
 
-  # def count_hits
-  #   @hits += 1
-  # end
+ def sunk
+   @size == @hits
+     true
+     # @floating = false
+ end
 
+ # def start_position
+
+ # end
+
+ # def size
+ # end
+
+
+ # def count_hits
+ #   @hits += 1
+ # end
 
 
 #   include Hit
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,74 +1,58 @@
 require "./lib/ship"
 
+
 describe Ship do
 
-  it "on initialize create a ship" do
-    ship = Ship.new(1)
-    expect(ship.floating?).to eq true
-  end
+describe "when initialized" do
 
-  it "on initialize create a ship with size" do
-    ship = Ship.new(1)
-    expect(ship.floating?).to eq true
-  end
+ it "create a ship" do
+   ship = Ship.new(1, 'A1')
+   expect(ship.sunk).to eq false
+ end
 
-  # it "on initialize has a position" do
-  #   ship = Ship.new 'A9'
-  #   expect(ship.position).to match(/^[A-J0-9]*$/)
-  # end
+ it "create a ship with size" do
+   ship = Ship.new(1, 'A1')
+   expect(ship.sunk).to eq false
+ end
 
-  it "can be hit" do
-    ship = Ship.new(1)
-    allow(ship).to receive(:recieve_hit).and_return(true)
-  end
+ it "has a position" do
+   ship = Ship.new(1, 'A1')
+   expect(ship.start_position).to match(/^[A-J0-9]*$/)
+ end
 
-  it "can't be hit when missed" do
-    ship = Ship.new(1)
-    allow(ship).to receive(:recieve_hit).and_return(false)
-  end
+end
 
-  it "can be missed" do
-    ship = Ship.new(1)
-    expect(ship.floating?).to eq true
-  end
+ it "can be hit" do
+   ship = Ship.new(1, 'A1')
+   allow(ship).to receive(:recieve_hit).and_return(true)
+ end
 
-  it "is sunk" do
-    ship = Ship.new(1)
-    ship.recieve_hit
-    expect(ship.sunk).to eq true
-  end
+ it "can't be hit when missed" do
+   ship = Ship.new(1, 'A1')
+   allow(ship).to receive(:recieve_hit).and_return(false)
+ end
 
+ it "can be missed" do
+   ship = Ship.new(1, 'A1')
+   expect(ship.sunk).to eq false
+ end
 
-
-  # it "record number of hits" do
-  #   ship = Ship.new
-  #   expect { hits +=1 }.to change{hits}.by(1)
-  # end
+ it "is sunk" do
+   ship = Ship.new(1, 'A1')
+   ship.recieve_hit
+   expect(ship.sunk).to eq true
+ end
 
 
-
+ # it "record number of hits" do
+ #   ship = Ship.new
+ #   expect { hits +=1 }.to change{hits}.by(1)
+ # end
 
 
 
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -89,7 +73,6 @@ end
 #     ship = Ship.new 'A1'
 #     expect(ship.hit).to eq true
 #   end
-
 
 
 #   # it "knows own size" do
