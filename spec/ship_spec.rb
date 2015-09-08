80cd30c653a1449fab7c8391a62519d27d21a840
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-08 20:12:13 +0100

    Added doubles

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,37 +2,29 @@ require "./lib/ship"
 
 describe Ship do
 
-  describe "when initialized" do
+  let(:ship){Ship.new(1)}
 
-    it "create a ship" do
-      ship = Ship.new(1)
-      expect(ship.floating?).to eq true
-    end
-
+  context "when initialized" do
     it "create a ship with size" do
-      ship = Ship.new(1)
-      expect(ship.floating?).to eq true
+      expect(ship.size).to eq 1
     end
-
   end
 
   it "can be hit" do
-    ship = Ship.new(1)
+    ship.recieve_hit
     allow(ship).to receive(:recieve_hit).and_return(true)
   end
 
   it "can't be hit when missed" do
-    ship = Ship.new(1)
+    ship.recieve_hit
     allow(ship).to receive(:recieve_hit).and_return(false)
   end
 
   it "can be missed" do
-    ship = Ship.new(1)
     expect(ship.floating?).to eq true
   end
 
   it "is sunk" do
-    ship = Ship.new(1)
     ship.recieve_hit
     expect(ship.sunk).to eq true
   end
@@ -65,5 +57,5 @@ describe Ship do
     end
 
   end
-
+  
 end
