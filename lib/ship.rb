29258ff93c57384c4045fcd68c11d53ac2bbbf73
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-08 19:51:44 +0100

    Added different size ships

--- lib/ship.rb
+++ lib/ship.rb
@@ -21,4 +21,24 @@ class Ship
     @size == @hits
   end
 
+  def self.patrol_boat
+    new 2
+  end
+
+  def self.destroyer
+    new 3
+  end
+
+  def self.submarine
+    new 3
+  end
+
+  def self.battleship
+    new 4
+  end
+
+  def self.aircraft_carrier
+    new 4
+  end
+
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,6 +1,5 @@
 require "./lib/ship"
 
-
 describe Ship do
 
   describe "when initialized" do
@@ -14,6 +13,7 @@ describe Ship do
       ship = Ship.new(1)
       expect(ship.floating?).to eq true
     end
+
   end
 
   it "can be hit" do
@@ -35,6 +35,35 @@ describe Ship do
     ship = Ship.new(1)
     ship.recieve_hit
     expect(ship.sunk).to eq true
+  end
+
+  describe "can create different ships" do
+
+    it "can create patrol boat" do
+      pb = Ship.patrol_boat
+      expect(pb.size).to eq 2
+    end
+
+    it "can create destroyer" do
+      dest = Ship.destroyer
+      expect(dest.size).to eq 3
+    end
+
+    it "can create submarine" do
+      sub = Ship.submarine
+      expect(sub.size).to eq 3
+    end
+
+    it "can create battleship" do
+      bs = Ship.battleship
+      expect(bs.size).to eq 4
+    end
+
+    it "can create aircraft carrier" do
+      ac = Ship.aircraft_carrier
+      expect(ac.size).to eq 4
+    end
+
   end
 
 end
